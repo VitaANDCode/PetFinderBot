a6587947f563458452a0b2fb0c9747b8f074b848
VitaC0de <vitacode008@gmail.com>
2023-11-25 15:52:15 +0500

    site link

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -1,8 +1,12 @@
 from aiogram import Bot
 from aiogram.types import Message
 from core.keyboards.Reply import reply_keyboard
+from core.keyboards.Inline import site_link
 
 
 async def get_start(message: Message, bot: Bot):
     await bot.send_message(message.from_user.id, f'Привет, {message.from_user.first_name}! Представляю вашему вниманию сервис для идентификации пропавших животных.', reply_markup=reply_keyboard)
 
+
+async def get_links(message: Message, bot : Bot):
+    await bot.send_message(message.from_user.id, f'Перейти на наш сайт вы можете по ссылке ниже',reply_markup=site_link)
---
+++ core/keyboards/Inline.py
@@ -0,0 +1,8 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+site_link = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(
+            text='Перейти на сайт',
+            url='https://animals.admhmao.ru/animals/#/dashboard'
+        )
+    ]])
