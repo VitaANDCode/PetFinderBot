002a5cc5f8db86029bc65d5f2004a67dd9a5d4a4
VitaC0de <vitacode008@gmail.com>
2023-11-25 14:41:36 +0500

    null

--- core/keyboards/Reply.py
+++ core/keyboards/Reply.py
@@ -1,5 +1,5 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 reply_keyboard = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Найти пропавшее Животное'), KeyboardButton (text='Наш Сайт')]],
+    [KeyboardButton(text='Найти пропавшее Животное'), KeyboardButton (text='Наш Сайт',  callback_data="get_site")]],
     resize_keyboard=True, one_time_keyboard=True, input_field_placeholder='Выбери кнопку', selective=True)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, Dispatcher
-import asyncio
-
 from core.handlers.basic import get_start
+
+import asyncio
 
 TOKEN_API = "6742857616:AAE9cbUJBzUnF0v7p9XmKelpCfTzb9mykFw"
 
