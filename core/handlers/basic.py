a3a6f470af125b9847a82237ff964f8de6529f15
VitaC0de <vitacode008@gmail.com>
2023-11-26 03:00:28 +0500

    info del

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -5,16 +5,10 @@ import datetime
 
 async def get_start(message: Message, bot: Bot):
     await bot.send_message(message.from_user.id,f'Привет, {message.from_user.first_name}! Представляю вашему вниманию сервис для идентификации пропавших животных.',reply_markup= site_link)
-    await get_info(message, bot)
-
-# async def get_start(message: Message, bot: Bot):
-#     await bot.send_message(message.from_user.id,f'Привет, {message.from_user.first_name}! Представляю вашему вниманию сервис для идентификации пропавших животных.')
-#     await get_info(message, bot)
 
 
 async def get_site(message: Message, bot: Bot):
     await bot.send_message(message.from_user.id, f'Перейти на наш сайт вы можете по ссылке ниже', reply_markup=site_link)
-    await get_info(message, bot)
 
 
 async def search_pet(message: Message, bot: Bot):
@@ -24,17 +18,3 @@ async def search_pet(message: Message, bot: Bot):
 async def load_photo(message: Message):
     if message.content_type == 'photo':
         await message.bot.download(message.photo[-1], r"C:\Users\User\Desktop\PetFinderBot\core\handlers\Photo\\" + ''.join(str(datetime.datetime.now()).split(':')) + ".png")
-
-
-
-
-
-
-
-
-
-
-
-
-async def get_info(message: Message, bot: Bot):
-    await bot.send_message(message.from_user.id, f'Список команд:\n/search - поиск питомца\n/site - переход на наш сайт\n/info - список команд')
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from core.handlers.basic import get_start
 from core.handlers.basic import search_pet
-from core.handlers.basic import get_info
 from core.handlers.basic import get_site
 from core.handlers.basic import load_photo
 from aiogram.filters.callback_data import CallbackQuery
@@ -25,11 +24,7 @@ async def start():
     dp.message.register(get_start, Command('start'))
     dp.message.register(get_site, Command('site'))
     dp.message.register(search_pet, Command('search'))
-    dp.message.register(get_info, Command('info'))
     dp.message.register(load_photo)
-
-
-
 
     try:
         await dp.start_polling(bot)
